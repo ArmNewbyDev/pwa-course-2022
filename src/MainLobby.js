a1766e5d0d44c692bdc2b55a748b60b001751d6d
ArmNewbyDev <arm123009@gmail.com>
2022-02-21 21:56:44 +0700

    Add New Page (mainlobby)

---
+++ src/MainLobby.js
@@ -0,0 +1,14 @@
+import './css/MainLobby.css';
+
+
+function MainLobby(){
+    return(
+        <div className="MainLobby">
+            <div className='BigText'> Lobby </div>
+            <div className='MoreBig'> Blank Text </div>
+        </div>
+        
+    )
+}
+
+export default MainLobby;
